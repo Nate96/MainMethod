9914c25205f98d45c19799eb1d9e9a4829f159ae
Nate Braukhoff <braukhoff96@gmail.com>
2018-07-24 14:19:25 -0500

    finished method

--- MainMethod.py
+++ MainMethod.py
@@ -1,36 +1,69 @@
-def add_(x, y):
-    return x + y
+def add(x, y):
+    # 1.)
+    # we want to add x and y together and return that value
+    # add x and y together
+    # raze hang for Nate to check your work. MUST TO THIS TO CONTINUE
+    return
 
 
 def subtract(x, y):
-    return x - y
+    # 2.)
+    # we want to subtract x and y together and return that value
+    # subtract x and y together
+    # raze hand for Nate to check your work. MUST TO THIS TO CONTINUE
+    return
 
 
-def is_higher_than_ten_(x):
+def is_higher_than_ten(x):
     return x > 10
 
 
-def check_operation(operation):
-    return operation == 'a'
+def check_for_add_operation(operation):
+    return operation == 'a' or operation == "A"
+
+
+def check_for_subtract_operation(operation):
+    return operation == 's' or operation == "S"
 
 
 if __name__ == "__main__":
-    num1 = int(input("Enter a number: "))
+    num1 = int(input("enter in fist number: "))
 
-    if is_higher_than_ten_(num1):
-        print("The number is higher than 10")
+    # 3.)
+    # check to see if that number is greater than ten
+    # HINT: call the proper method
+    #       don't forget the : at the end of the statement
+    if
+        # 4.)
+        # print out "Then number is higher than 10"
+
     elif num1 == 10:
         print("The number is 10")
     else:
-        print("The number lower than 10")
+        # 5.)
+        # print out "The number is lower than 10"
 
     num2 = int(input("Enter a second number: "))
 
     symbol = input("press a for add or press s for subtract: ")
 
-    if check_operation(symbol):
-        z = add_(num1, num2)
-        print(z)
-    else:
-        z = subtract(num1, num2)
-        print(z)
+    # 6.)
+    # check to see if the user has entered 'a'
+    #HINT: call the right method
+    if
+        # 7.)
+        # Use the add method to add the num1 and num2 together.
+        # set it equal to z
+        z =
+        # 8.)
+        # Print out z
+        #HINT: use the print() method
+
+    # 9.) check to see if the user had entered 's'
+    if
+        # 10.)
+        # use the subtract method to subtract num1 and num2 together.
+        #set it equal to z
+        z =
+        # 11.)
+        # use the print() method to print out z
