8f89e68ecb9191d7894ce95f79580bff4c42e465
Nate Braukhoff <braukhoff96@gmail.com>
2018-07-24 14:16:33 -0500

    added directions

--- mainMethodTest.py
+++ mainMethodTest.py
@@ -1,20 +1,31 @@
 import unittest
-from mainMethod import object
+import mainMethod
 
 
 class TestMainMethod(unittest.TestCase):
-    def setUp(self):
-        self.object = object()
-
     def test_add(self):
-        self.assertEqual(5, self.object.add_(2, 3))
+        self.assertEqual(5, mainMethod.add(2, 3))
 
-        self.assertNotEqual(15, self.object.add_(5, 3))
+        self.assertNotEqual(15, mainMethod.add(5, 3))
 
-    def test_is_higher_than_ten_(self):
-        self.assertTrue(object.is_higher_than_ten_(13))
+    def test_subtract(self):
+        self.assertEqual(10, mainMethod.subtract(15, 5))
 
-        self.assertFalse(object.is_higher_than_ten_(10))
+    def test_is_higher_than_ten(self):
+        self.assertTrue(mainMethod.is_higher_than_ten(13))
 
-        self.assertFalse(object.is_higher_than_ten_(5))
+        self.assertFalse(mainMethod.is_higher_than_ten(10))
 
+        self.assertFalse(mainMethod.is_higher_than_ten(5))
+
+    def test_check_add_operation(self):
+        self.assertTrue(mainMethod.check_for_add_operation('a'))
+        self.assertTrue(mainMethod.check_for_add_operation('A'))
+
+        self.assertFalse(mainMethod.check_for_add_operation('s'))
+
+    def test_check_subtraction_operation(self):
+        self.assertTrue(mainMethod.check_for_subtract_operation('s'))
+        self.assertTrue(mainMethod.check_for_subtract_operation('S'))
+
+        self.assertFalse(mainMethod.check_for_subtract_operation('t'))
