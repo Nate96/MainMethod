ad8aa076764d6c4681e23a6d7f0f40674cd6efba
Nate Braukhoff <braukhoff96@gmail.com>
2018-07-24 13:47:21 -0500

    Completed the first build of the program. Going to refactor

--- MainMethod.py
+++ MainMethod.py
@@ -1,13 +1,36 @@
-class object():
-    @staticmethod
-    def add_(x, y):
-        return x + y
+def add_(x, y):
+    return x + y
 
-    @staticmethod
-    def is_higher_than_ten_(x):
-        return x > 10
+
+def subtract(x, y):
+    return x - y
+
+
+def is_higher_than_ten_(x):
+    return x > 10
+
+
+def check_operation(operation):
+    return operation == 'a'
 
 
 if __name__ == "__main__":
-    x = 60
-    x = 5
+    num1 = int(input("Enter a number: "))
+
+    if is_higher_than_ten_(num1):
+        print("The number is higher than 10")
+    elif num1 == 10:
+        print("The number is 10")
+    else:
+        print("The number lower than 10")
+
+    num2 = int(input("Enter a second number: "))
+
+    symbol = input("press a for add or press s for subtract: ")
+
+    if check_operation(symbol):
+        z = add_(num1, num2)
+        print(z)
+    else:
+        z = subtract(num1, num2)
+        print(z)
